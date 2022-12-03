ca119d5795dc9bd6b4c18e72f54c8963b070535f
AnneLonero <114631804+AnneLonero@users.noreply.github.com>
2022-12-03 13:34:15 -0800

    Adding the election analysis.

--- Python_practice.py
+++
@@ -1,5 +0,0 @@
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-for county in counties_dict:
-    print(counties_dict[county])
-
-    print(f"{candidate_name}: received {vote_percentage}% of the vote")
